5277e0dc926eb226ea0ea8c3d9945b43926c8f9c
krilooleh <mayorford777@gmail.com>
2022-10-02 14:23:51 +0300

    Add parcel + clean index.js

--- src/CardsAnimation.js
+++ src/CardsAnimation.js
@@ -65,17 +65,3 @@ export class CardsAnimate{
 
 }
 
-
-function animate(){
-    const sponsors_inner = document.querySelector(".sponsors_inner")
-    const sponsors_container = document.querySelector(".sponsors_cards")
-
-    const animation = new CardsAnimate(sponsors_inner, sponsors_container, 1)
-    animation.init()
-}
-
-animate()
-
-
-
-
