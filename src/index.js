f01b1da62d948f63942f128f94de649d6d5dc88b
krilooleh <mayorford777@gmail.com>
2022-10-26 18:48:45 +0300

    sidebar animation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import {CardsAnimate} from "./CardsAnimation"
 import {initMap} from "./plugins"
 
+//Sidebar behavior
+function sidebarLinks(){
+    const links = document.querySelectorAll(`.sidebar_link`)
+
+    links.forEach(link =>{
+        link.addEventListener("click", (e) => {
+            document.querySelectorAll(`.sidebar_link`).forEach((link) => {
+                link.parentElement.classList.remove('sidebar_link_visited')
+            })
+            e.target.parentElement.classList.add("sidebar_link_visited")
+        })
+    })
+}
+sidebarLinks()
+
+
 //Animate intro lines ["AIS - it's when" block]
 function animateIntroLines(animatedBlock){
     const introLines = document.querySelectorAll(".ais-info_label")
