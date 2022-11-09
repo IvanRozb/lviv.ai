0af2a17580aae2116611f6f21810150ba2bccbd6
krilooleh <mayorford777@gmail.com>
2022-11-09 13:32:46 +0200

    add applicant sidebar links gradient when they are in active state

--- src/applicant.js
+++ src/applicant.js
@@ -6,11 +6,21 @@ function applicantSidebar(){
         link.addEventListener("click", (e) => {
 
             const sections = document.querySelectorAll(`section`)
-            debugger
+
             sections.forEach((section) => {
                 section.style.display = "none"
                 if(section.classList.contains(e.target.parentNode.dataset.link)){
                     section.style.display = "block"
+                }
+            })
+
+            const links = document.querySelectorAll(`.sidebar_list > *`)
+            debugger
+            links.forEach((link) => {
+                if(link == e.target.parentNode){
+                    link.classList.add("active")
+                }else{
+                    link.classList.remove("active")
                 }
             })
 
