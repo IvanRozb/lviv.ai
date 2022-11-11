10936d22c21bfc0a371cff0c4036f8a13fa7030d
huan_rozb <ivanrozb4225@gmail.com>
2022-11-11 12:13:27 +0200

    added for applicant functionality

--- src/fetch.js
+++ src/fetch.js
@@ -11,7 +11,7 @@ function changeTagIfNeeded(tag, element) {
     return tagCopy;
 }
 
-class Fetch{
+class Fetch {
     static objTags = {
         name: 'h3',
         title: 'h3',
@@ -21,10 +21,9 @@ class Fetch{
         logoUrl: 'img',
         summary: 'p',
         video: 'a',
-
     };
 
-    static async getCollection(collection, language){
+    static async getCollection(collection, language) {
         let result = ''
         await fetch(`http://54.93.52.237/aiwebsite/${collection}?language=${language}`,
             {
@@ -41,25 +40,21 @@ class Fetch{
                     for (const name in data[i]) {
                         console.log(data[i][name]);
 
-                        if(Array.isArray(data[i][name])) {
+                        if (Array.isArray(data[i][name])) {
                             for (const element of data[i][name]) {
                                 let tag = changeTagIfNeeded((this.objTags)[element], element);
-                                
+
                                 if (tag === 'img')
                                     result += `<${tag} src = '${element}' style='display: block;'></${tag}>`;
                                 else if (name != 'id' && name != 'language')
                                     result += `<${tag}>${data[i][name]}</${tag}><br>`;
                             }
-                        }
-                        else if(typeof data[i][name] === 'object')
-                        {
+                        } else if (typeof data[i][name] === 'object') {
                             for (const element in data[i][name]) {
                                 let tag = changeTagIfNeeded((this.objTags)[element], element);
                                 result += `<${tag}>${element}:${data[i][name][element]}</${tag}><br>`;
                             }
-                        }
-                        else
-                        {
+                        } else {
                             let tag = changeTagIfNeeded((this.objTags)[data[i][name]], data[i][name]);
                             if (tag === 'img')
                                 result += `<${tag} src = '${data[i][name]}' style='display: block;'></${tag}>`
@@ -70,6 +65,58 @@ class Fetch{
                 }
             });
         return result
+    }
+
+    static async getApplicants(language) {
+        let result = '';
+        await fetch(`http://54.93.52.237/aiwebsite/Applicants?language=${language}`,
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            })
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                for (let i = 0; i < data.length; i++) {
+                    for (const name in data[i]) {
+                        const element = data[i][name]
+                        console.log(element)
+                        switch (name){
+                            case "explanations":
+                                for (const explanation in element) {
+                                    result += `<div class="docs_box">`;
+                                    result += `<p class="docs_list_title">&{explanation.title}</p>`;
+                                    result += `<ul class="docs_list">`;
+                                    for (const item in explanation.items) {
+                                        result += `<li class="docs_list_items"><h6 class="docs_list_text">&{item}</h6></li>`
+                                    }
+                                    result += `</ul>`
+                                    result += `</div>`
+                                    if (explanation.photo != null) {
+                                        result += ` <div class="docs_cabinet">
+                                                        <img class="docs_cabinet_img" src="${explanation.photo}" alt="form">
+                                                    </div>`
+                                    }
+                                }
+                                break;
+                            case "educationCosts":
+                                for (const row in element) {
+                                    result+=`<tr class="pay_table_row">`
+                                    result+=`<th class="pay_table_value ">&{row.degree}</th>`
+                                    result+=`<th class="pay_table_value ">&{row.term}</th>`
+                                    result+=`<th class="pay_table_value ">&{row.capacity}</th>`
+                                    result+=`</tr>`
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            });
     }
 }
 //Render data
