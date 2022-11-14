157b6f287ea780be752989a18df3feb7ad9dc513
huan_rozb <ivanrozb4225@gmail.com>
2022-11-14 18:18:57 +0200

    updated static applicant made applicant.js more suitable

--- src/applicant.js
+++ src/applicant.js
@@ -1,55 +1,26 @@
-// Application sidebar behavior
-function sidebarBehavior(){
-    const sidebar = document.querySelector(`.sidebar_list`);
+function navigationListBehavior(/*string*/containerSelector, /*string*/blocksSelector, /*string*/linksSelector){
+    const sidebar = document.querySelector(containerSelector);
     sidebar.childNodes.forEach(link => {
         link.addEventListener("click", e => {
-            const sections = document.querySelectorAll(`section`)
+            const sections = document.querySelectorAll(blocksSelector);
             sections.forEach((section) => {
                 section.style.display = "none"
-                if(section.classList.contains(e.target.parentNode.dataset.link)){
+                if(section.classList.contains(e.target.dataset.link)){
                     section.style.display = "block"
-                }
-            })
-
-            const links = document.querySelectorAll(`.sidebar_list > li`)
-            links.forEach((link) => {
-                if(link === e.target.parentNode){
-                    link.classList.add("active")
-                }else{
-                    link.classList.remove("active")
-                }
-            })
-        })
-    })
-}
-
-// Application dates of subjects behavior
-function subjectsDatesBehavior(){
-    const dates = document.querySelector(`.subjects_dates`);
-    dates.childNodes.forEach(link =>{
-        link.addEventListener("click", e => {
-            const images = document.querySelectorAll(`.subjects_images > img`);
-            images.forEach((image) => {
-                image.style.display = "none";
-                if(image.classList.contains(e.target.dataset.link)){
-                    image.style.display = "block";
                 }
             });
 
-            const dates = document.querySelectorAll(`.subjects_date`);
-            dates.forEach((date) => {
-                if(date === e.target){
-                    e.target.classList.add("color_letter_red");
+            const links = document.querySelectorAll(linksSelector);
+            links.forEach((link) => {
+                if(link === e.target){
+                    link.classList.add("active");
                 }else{
-                    date.classList.remove("color_letter_red");
+                    link.classList.remove("active");
                 }
-            })
+            });
         })
-    })
+    });
 }
 
-function applicantBehavior(){
-    sidebarBehavior();
-    subjectsDatesBehavior();
-}
-applicantBehavior();
+navigationListBehavior(`.sidebar_list`, `section`, `.sidebar_list > li`);
+navigationListBehavior(`.subjects_dates`, `.subjects_images > img`, `.subjects_date`);
